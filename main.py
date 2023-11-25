8ab16e2c5ea74eaa04da6e6ee57baf34535a0855
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-11-25 11:54:59 +0100

    complete Turtle Race project

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from CoffeeMachine.main import start as coffee_machine
 from CoffeeMachineOOP.main import start as coffee_machine_oop
 from QuizGame.main import start as quiz_game
 from HirsPainting.main import start as hirs_painting
+from TurtleRace.main import start_race as turtle_race
 
 
 print("1. Coffee Machine")
 print("2. Coffee Machine OOP")
 print("3. Quiz Game")
 print("4. Hirs Painting")
+print("4. Turtle Race")
 
 user_option = input("What project do you want to test? (Insert number) ")
 
@@ -20,5 +22,7 @@ match user_option:
         quiz_game()
     case "4":
         hirs_painting()
+    case "5":
+        turtle_race()
     case _:
         print("Bye!")
