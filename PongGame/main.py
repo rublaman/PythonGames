cb6874e47ca46929d90d804bf79bbf12403c800b
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-11-27 16:27:45 +0100

    add paddle class

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen
+from PongGame.paddle import Paddle
+import time
 
 
 def start():
@@ -7,13 +9,21 @@ def start():
     screen.setup(width=800, height=600)
     screen.bgcolor("black")
     screen.title("Pong")
+    screen.tracer(0)
 
+    right_paddle = Paddle((350, 0))
+    left_paddle = Paddle((-350, 0))
 
+    screen.listen()
 
+    screen.onkey(right_paddle.up, "Up")
+    screen.onkey(right_paddle.down, "Down")
+    screen.onkey(left_paddle.up, "Up")
+    screen.onkey(left_paddle.down, "Down")
 
-
-
-
-
+    is_game_on = True
+    while is_game_on:
+        screen.update()
+        time.sleep(0.1)
 
     screen.exitonclick()
---
+++ PongGame/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+UP = 90
+DOWN = 270
+STEP = 20
+
+class Paddle(Turtle):
+
+    def __init__(self, position: tuple):
+        super().__init__()
+        self.goto(position)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
