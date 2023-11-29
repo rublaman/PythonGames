ad1328410e7ffe5d6df35b6e3ad0f46cf2656870
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-11-29 09:31:27 +0100

    add initial classes

--- TurtleCrossing/scoreboard.py
+++ TurtleCrossing/scoreboard.py
@@ -1,5 +1,17 @@
+from turtle import Screen
+import time
+
 FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard:
-    pass
+
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.tracer(0)
+
+    is_game_on = True
+    while is_game_on:
+        time.sleep(0.1)
+        screen.update()
+
