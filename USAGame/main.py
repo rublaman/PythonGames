040b6f4c504b264bfd4e818fcabb346f2ad7c77e
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-12-02 13:36:56 +0100

    add question loop and read from csv file

--- USAGame/main.py
+++ USAGame/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import pandas as pd
 
 screen = Screen()
 turtle = Turtle()
@@ -8,4 +9,23 @@ image_path = "blank_states_img.gif"
 screen.addshape(image_path)
 turtle.shape(image_path)
 
-answer_input = screen.textinput(title="Guess the State", prompt="What's another states name?")
+df = pd.read_csv('50_states.csv')
+
+is_game_on = True
+guessed_states = []
+
+
+while is_game_on or len(guessed_states) < 50:
+    answer_input = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another states name?").title()
+    state_with_cords = df[df["state"] == answer_input]
+
+    if not state_with_cords.empty:
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(state_with_cords.iloc[0]["x"]), int(state_with_cords.iloc[0]["y"]))
+        t.write(state_with_cords.iloc[0]["state"])
+        guessed_states.append(state_with_cords.iloc[0]["state"])
+
+    if answer_input == "Exit":
+        is_game_on = False
