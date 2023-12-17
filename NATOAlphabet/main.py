4195127e86dddc995c4528608ced648dce37638e
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-12-17 20:36:51 +0100

    add try except in NATO Alphabet

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -6,9 +6,18 @@ import pandas as pd
 nato_phonetic_dataframe = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_dictionary = {row["letter"]: row["code"] for (index, row) in nato_phonetic_dataframe.iterrows()}
 
+
 # Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("What is your name: ").upper()
-result = [nato_phonetic_dictionary[letter] for letter in user_input]
+def nato_alphabet():
+    user_input = input("What is your name: ").upper()
 
-print(result)
+    try:
+        result = [nato_phonetic_dictionary[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_alphabet()
+    else:
+        print(result)
+
+nato_alphabet()
