b91855efe433c298163d49d41495a2a692073c87
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-12-19 18:19:28 +0100

    update canvas

--- FlashCardEnglishSpanish/main.py
+++ FlashCardEnglishSpanish/main.py
@@ -21,9 +21,11 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 img_card_front = PhotoImage(file=img_path_card_front)
 canvas.create_image(400, 263, image=img_card_front)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
-# Labels
+
 
 # Buttons
 img_wrong = PhotoImage(file=img_path_wrong)
