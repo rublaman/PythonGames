79d883c842a3a5d85294e8aa9b03d8d856aa4928
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2023-12-20 12:18:06 +0100

    complete Flash Card project

--- FlashCardEnglishSpanish/main.py
+++ FlashCardEnglishSpanish/main.py
@@ -1,21 +1,30 @@
 import os
 from tkinter import *
+
+import pandas
 import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+dict_list = {}
 
 # ---------------------------- FILE & FILE PATHS ---------------------- #
 current_dir = os.path.dirname(__file__)
 csv_english_word = os.path.join(current_dir, 'data/english_words.csv')
+csv_words_to_learn = os.path.join(current_dir, 'data/words_to_learn.csv')
 img_path_card_back = os.path.join(current_dir, 'images/card_back.png')
 img_path_card_front = os.path.join(current_dir, 'images/card_front.png')
 img_path_right = os.path.join(current_dir, 'images/right.png')
 img_path_wrong = os.path.join(current_dir, 'images/wrong.png')
 
-df = pd.read_csv(csv_english_word)
-dict_list = df.to_dict(orient="records")
-current_card = {}
+try:
+    df = pd.read_csv(csv_words_to_learn)
+except FileNotFoundError:
+    original_data = pandas.read_csv(csv_english_word)
+    dict_list = original_data.to_dict(orient="records")
+else:
+    dict_list = df.to_dict(orient="records")
 
 
 # ---------------------------- NEW WORD -------------------------------- #
@@ -26,17 +35,24 @@ def generate_new_word():
     window.after_cancel(flip_timer)
     current_card = random.choice(dict_list)
     canvas.itemconfig(canvas_image, image=img_card_front)
-    canvas.itemconfigure(canvas_language, text="English")
-    canvas.itemconfigure(canvas_word, text=current_card["English"])
+    canvas.itemconfig(canvas_language, text="English", fill="black")
+    canvas.itemconfig(canvas_word, text=current_card["English"], fill="black")
 
     flip_timer = window.after(3000, show_result)
 
 
-# ---------------------------- TRANSLATE WORD -------------------------- #
+# ---------------------------- TRANSLATE WORD -----------+--------------- #
 def show_result():
-    canvas.itemconfigure(canvas_language, text="Spanish")
-    canvas.itemconfigure(canvas_word, text=current_card["Spanish"])
+    canvas.itemconfig(canvas_language, text="Spanish", fill="white")
+    canvas.itemconfig(canvas_word, text=current_card["Spanish"], fill="white")
     canvas.itemconfig(canvas_image, image=img_card_back)
+
+
+def is_known_card():
+    dict_list.remove(current_card)
+    data = pd.DataFrame(dict_list)
+    data.to_csv(csv_words_to_learn, index=False)
+    generate_new_word()
 
 
 # ---------------------------- UI SETUP -------------------------------- #
@@ -44,8 +60,6 @@ def show_result():
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-
 
 # Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -62,11 +76,10 @@ button_wrong = Button(image=img_wrong, highlightthickness=0, command=generate_ne
 button_wrong.grid(column=0, row=1)
 
 img_right = PhotoImage(file=img_path_right)
-button_wrong = Button(image=img_right, highlightthickness=0, command=generate_new_word)
+button_wrong = Button(image=img_right, highlightthickness=0, command=is_known_card)
 button_wrong.grid(column=1, row=1)
 
 flip_timer = window.after(3000, func=show_result)
 generate_new_word()
-
 
 window.mainloop()
