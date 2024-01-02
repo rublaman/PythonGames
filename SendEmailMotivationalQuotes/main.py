1698359fe48e9725408ffcfc17b04de4d127002b
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-01-02 22:45:40 +0100

    add smtplib part to send emails

--- SendEmailMotivationalQuotes/main.py
+++ SendEmailMotivationalQuotes/main.py
@@ -1,4 +1,19 @@
+import smtplib
+
 from configparser import  ConfigParser
 
+
 config = ConfigParser()
-config.read('../config.cfg')
+config.read("../config.cfg")
+email_user = config["email"]["account"]
+email_secret = config["email"]["secret"]
+
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=email_user, password=email_secret)
+    connection.sendmail(
+        from_addr=email_user,
+        to_addrs="rubenblancomanzano@gmail.com",
+        msg="Subject:Hi!!\n\nThis is the body of the mail"
+    )
