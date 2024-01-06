75ff5ba58ea4199eb7456322a469aeb02d946a00
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-01-06 12:15:43 +0100

    fix typo

--- TriviaApp/data.py
+++ TriviaApp/data.py
@@ -6,6 +6,6 @@ parameters = {
     "type": "boolean"
 }
 
-r = requests.get("https://opentdb.com/api.php", params=parametersf)
+r = requests.get("https://opentdb.com/api.php", params=parameters)
 r.raise_for_status()
 question_data = r.json()["results"]
