120d5df6726f5b256c02fc459e85552d47617030
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-01-10 22:12:49 +0100

    start Stock trading alert SMS

---
+++ StockTradingAlertSMS/main.py
@@ -0,0 +1,40 @@
+import os
+from twilio.rest import Client
+import requests
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+news_api_key = os.environ['NEWSAPI_API_KEY']
+
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+news_response = requests.get(f"https://newsapi.org/v2/top-headlines?q=tesla&apiKey={news_api_key}")
+print(news_response.json())
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
+# client = Client(account_sid, auth_token)
+# message = client.messages.create(
+#                               from_='+',
+#                               body='Hi there',
+#                               to='+'
+#                           )
+# print(message.sid)
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
