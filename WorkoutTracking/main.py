875ea857dd7a5d5fca5b02703b17834eaf080902
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-01-17 23:06:26 +0100

    fix request

--- WorkoutTracking/main.py
+++ WorkoutTracking/main.py
@@ -7,16 +7,24 @@ APP_ID = os.environ.get("APP_ID", "APP ID it doesn't exist")
 API_KEY = os.environ.get("API_KEY", "API_KEY it doesn't exist")
 
 # 2 Get Exercise stats with NLQ
-response = requests.post(
-    'https://trackapi.nutritionix.com/v2/natural/exercise',
-    headers={
-        "x-app-id": APP_ID,
-        "x-app-key": API_KEY
-    }, params={
-        "query": "swam for 1 hour"
-    })
+url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+headers = {
+    "Content-Type": "application/json",
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
 
-print(response)
+data = {
+    "query": "swam for 1 hour"
+}
+
+response = requests.post(url, headers=headers, json=data)
+if response.status_code == 200:
+    response = response.json()
+    print(response)
+else:
+    print(f"Error in the request. Status code: {response.status_code}")
+    print(response.text)
 
 # 3 Setup Google Sheet with Sheety
 
