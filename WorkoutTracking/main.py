ff38439f3a6693edb8069484d3b2aea07da13a69
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-01-17 22:56:02 +0100

    add api nutritionx and environment variables

--- WorkoutTracking/main.py
+++ WorkoutTracking/main.py
@@ -1,6 +1,22 @@
-# 1 Setup API Credentials
+import requests
+import datetime
+import os
+
+# nutritionix
+APP_ID = os.environ.get("APP_ID", "APP ID it doesn't exist")
+API_KEY = os.environ.get("API_KEY", "API_KEY it doesn't exist")
 
 # 2 Get Exercise stats with NLQ
+response = requests.post(
+    'https://trackapi.nutritionix.com/v2/natural/exercise',
+    headers={
+        "x-app-id": APP_ID,
+        "x-app-key": API_KEY
+    }, params={
+        "query": "swam for 1 hour"
+    })
+
+print(response)
 
 # 3 Setup Google Sheet with Sheety
 
