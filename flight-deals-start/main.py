559640538b08d44fd0203d1ad2b620478f033be5
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-02-01 23:09:21 +0100

    start app

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,1 +1,12 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+from flight_data import FlightData
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+def start_app():
+    pass
+
+if __name__ == "__main__":
+    start_app()
