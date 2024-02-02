77f33308be6959e7e55a44b22ba0de494d9e99f0
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-02-02 23:32:45 +0100

    add twilio client

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,18 @@
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+number_twilio = os.environ['TWILIO_PHONE_NUMBER']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Test",
+                     from_=f'{number_twilio}',
+                     to='+01000000000'
+                 )
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     pass
