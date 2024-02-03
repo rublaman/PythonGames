4badb6a1a892dd339d1bfa3cb46e58e0ceaa68cd
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-02-03 22:55:34 +0100

    update send and build message methods

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -6,17 +6,21 @@ class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
 
     def __init__(self, target_phone_number: str):
-        self.phone_number = target_phone_number
+        self.target_phone_number = target_phone_number
 
-    def send_mail(self) -> None:
+    def send_mail(self, flight_detail: dict) -> None:
         account_sid = os.environ['TWILIO_ACCOUNT_SID']
         auth_token = os.environ['TWILIO_AUTH_TOKEN']
         number_twilio = os.environ['TWILIO_PHONE_NUMBER']
         client = Client(account_sid, auth_token)
+        body_message = self.__build_message(flight_detail)
 
         message = client.messages \
             .create(
-                body="Test",
+                body=body_message,
                 from_=f'{number_twilio}',
-                to=f'{self.phone_number}'
+                to=f'{self.target_phone_number}'
             )
+
+    def __build_message(self, flight_detail: dict) -> str:
+        return f"Your flight from XXX will be {flight_detail}"
