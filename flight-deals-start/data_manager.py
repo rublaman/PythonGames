d81f19d14b87dfd15e9eb9cd07fe66329bcdf007
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-02-03 23:16:48 +0100

    start DataManager class

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,5 +1,18 @@
 import requests
+import os
+from datetime import datetime
+
 
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        pass
+
+    def get_flights_from_google_sheet(self):
+        SHEETY_API_URL = "https://api.sheety.co/----------/workoutTracking/flights"
+
+        headers = {
+            'Authorization': f'Basic ---------'
+        }
+
+        response = requests.post(SHEETY_API_URL, headers=headers)
