e357010e42374d60f87def48062f8e3d785ee68f
Ruben Blanco Manzano <rubenblancomanzano@gmail.com>
2024-02-27 23:08:23 +0100

    add methods in FlightSearch

--- FlightDealsStart/flight_search.py
+++ FlightDealsStart/flight_search.py
@@ -7,12 +7,20 @@ class FlightSearch:
     # Docu -> https://tequila.kiwi.com/portal/docs/tequila_api/search_api
 
     def __init__(self, api_key: str):
-        self.__API_URL = f"https://api.tequila.kiwi.com/v2/search?fly_from={departure}&fly_to={arrival}"
+        self.__API_URL = f"https://api.tequila.kiwi.com/v2/search?"
         self.__HEADERS = {
             'Authorization': f'Basic {api_key}'
         }
 
-    def get_flight_price(self, info: dict) -> dict:
+    def __create_query(self, flight_details: dict) -> str:
+
+        return ""
+
+
+    def get_flight_price(self, flight_details: dict) -> dict:
+
+        query = self.__create_query()
+
         try:
             response = requests.get(self.__API_URL, headers=self.__HEADERS)
             response.raise_for_status()
@@ -20,3 +28,7 @@ class FlightSearch:
             return data
         except requests.exceptions.HTTPError as err:
             print(f"Error in request: {err}")
+
+
+test = FlightSearch(api_key="")
+print(test.get_flight_price())
